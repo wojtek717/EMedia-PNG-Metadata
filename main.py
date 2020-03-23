17d96c57514b7c75af72f6259de1da0a6a8125c6
wojtek717 <wojciechkonury@gmail.com>
2020-03-23 19:33:50 +0100

    Read chunks & fix 4issue

--- main.py
+++ main.py
@@ -1,19 +1,62 @@
 import binascii
 
+class Chunk:
+    def __init__(self, lengthArray, typeArray, dataArray, crcArray, nextChunkIndex):
+        self.lengthArray = lengthArray
+        self.typeArray = typeArray
+        self.dataArray = dataArray
+        self.crcArray = crcArray
+        self.nextChunkIndex = nextChunkIndex
+
+        self.calculateNextChunkIndex(self.nextChunkIndex)
+
+        print('Chunk type ->' + bytearray(typeArray).decode('utf-8'))
+
+    # IEND chunk is the last one so we need to point out that 
+    def calculateNextChunkIndex(self, nextChunkIndex):
+        if(bytearray(self.typeArray).decode('utf-8') == 'IEND'):
+            self.nextChunkIndex = -1    
+
 def read_chunk(bArray, startIndex):
     chunkIterator = startIndex
+    lengthArray = []
+    typeArray = []
+    dataArray = []
+    crcArray = []
     
     # Read and merge size Bytes
-    sizeArray = [0,0,0,0]
-    
-    sizeByte = 0
-    while sizeByte < 4:
-        sizeArray[sizeByte] = bArray[chunkIterator]
-        sizeByte += 1
+    lengthByte = 0
+    while lengthByte < 4:
+        lengthArray.append(bArray[chunkIterator])
+        lengthByte += 1
         chunkIterator += 1
     
-    chunkSize = sizeArray[3] | (sizeArray[2]<<8) | (sizeArray[1]<<16) | (sizeArray[0]<<24)
-    print(chunkSize)
+    # Merge 4*Byte into one 32binary number
+    chunkLength = lengthArray[3] | (lengthArray[2]<<8) | (lengthArray[1]<<16) | (lengthArray[0]<<24)
+
+    # Read chunk type
+    typeByte = 0
+    while typeByte < 4:
+        typeArray.append(bArray[chunkIterator])
+        typeByte += 1
+        chunkIterator += 1
+
+    # Read chunk data
+    dataByte = 0
+    while dataByte < chunkLength:
+        dataArray.append(bArray[chunkIterator])
+        dataByte += 1
+        chunkIterator += 1
+
+    # Read chunk CRC
+    crcByte = 0
+    while crcByte < 4:
+        crcArray.append(bArray[chunkIterator])
+        crcByte += 1
+        chunkIterator += 1
+
+    # Return chunk object
+    return(Chunk(lengthArray, typeArray, dataArray, crcArray, chunkIterator))
 
 def check_sygnature(bArray):
     sygnature = [137, 80, 78, 71, 13, 10, 26, 10]
@@ -26,18 +69,24 @@ def check_sygnature(bArray):
         sygnature_byte += 1
     print("Valid sygnature")
     return True        
-    
 
 
 def main():
     filename = 'ex1.png'
+    fileChunks = []
+
+    # Open and read file into byte array
     with open(filename, 'rb') as f:
         byteArray = f.read()
     #print(binascii.hexlify(byteArray))
 
-    check_sygnature(byteArray)
-    read_chunk(byteArray, 8)
+    if(check_sygnature(byteArray) != True):
+        return False
 
+    chunkIndex = 8
+    while chunkIndex != (-1):
+        fileChunks.append(read_chunk(byteArray, chunkIndex))
+        chunkIndex = fileChunks[-1].nextChunkIndex
 
 if __name__ == "__main__":
     main()
