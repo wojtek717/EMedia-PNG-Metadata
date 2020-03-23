5d977e1f22a01ee28ad022c5f09b93f00f35cd0a
wojtek717 <wojciechkonury@gmail.com>
2020-03-23 18:13:37 +0100

    Read and merge size Bytes in chunk

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import binascii
 
+def read_chunk(bArray, startIndex):
+    chunkIterator = startIndex
+    
+    # Read and merge size Bytes
+    sizeArray = [0,0,0,0]
+    
+    sizeByte = 0
+    while sizeByte < 4:
+        sizeArray[sizeByte] = bArray[chunkIterator]
+        sizeByte += 1
+        chunkIterator += 1
+    
+    chunkSize = sizeArray[3] | (sizeArray[2]<<8) | (sizeArray[1]<<16) | (sizeArray[0]<<24)
+    print(chunkSize)
+
 def check_sygnature(bArray):
     sygnature = [137, 80, 78, 71, 13, 10, 26, 10]
     sygnature_byte = 0
@@ -21,6 +36,7 @@ def main():
     #print(binascii.hexlify(byteArray))
 
     check_sygnature(byteArray)
+    read_chunk(byteArray, 8)
 
 
 if __name__ == "__main__":
