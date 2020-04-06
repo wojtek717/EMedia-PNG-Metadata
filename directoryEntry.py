f9281a1db4fe662c230840d77b5b984ebb93c8c9
wojtek717 <wojciechkonury@gmail.com>
2020-04-06 20:02:42 +0200

    Get DESize

--- directoryEntry.py
+++ directoryEntry.py
@@ -12,3 +12,24 @@ class DirectoryEntry:
     def getCount(self):
         count = int((self.countArray[3] | (self.countArray[2]<<8) | (self.countArray[1]<<16) | (self.countArray[0]<<24)))
         return count
+
+    def getDEsize(self):
+        tagTypeNumber = self.tagTypeArray[1] | (self.tagTypeArray[0]<<8)
+
+        if(tagTypeNumber == 1 or 
+            tagTypeNumber == 2 or 
+            tagTypeNumber == 6 or
+            tagTypeNumber == 7):
+            return 1
+        elif(tagTypeNumber == 3 or
+            tagTypeNumber == 8):
+            return 2
+        elif(tagTypeNumber == 4 or
+            tagTypeNumber == 9 or
+            tagTypeNumber == 11 or
+            tagTypeNumber == 13):
+            return 4
+        elif(tagTypeNumber == 5 or
+            tagTypeNumber == 10 or
+            tagTypeNumber == 12):
+            return 8
