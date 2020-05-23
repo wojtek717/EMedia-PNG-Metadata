98ff1521cf4411244b8ac1c31144dc0f952364fe
wojtek717 <wojciechkonury@gmail.com>
2020-05-23 12:21:07 +0200

    Encrypt should work

--- rsa.py
+++ rsa.py
@@ -18,45 +18,65 @@ def nwd(a, b):
         b = c
     return a
 
-def moduleInverse (a,n):
+def egcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = egcd(b % a, a)
+        return g, x - (b // a) * y, y
 
-    a0 = a
-    n0 = n
-    p0 = 0
-    p1 = 1
-    q = int(n0 / a0)
-    r = n0 % a0
 
-    while r > 0:
-        t = p0 - (q * p1)
-        if(t>=0):
-            t = t % n
-        else:
-            t = n - ((-t) % n )
-
-        p0 = p1
-        p1 = t
-        n0 = a0
-        a0 = r
-        q = int(n0 / a0)
-        r = n0 % a0
-
-    return p1
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
 
 def generate_keys():
     p = pnum.generate_prime_number()
     q = pnum.generate_prime_number()
 
+    print("P:::::")
+    print(p)
+    print("Q:::::")
+    print(q)
+
     n = p*q
     phi = (p-1)*(q-1)
+
+    print("phi:::::")
+    print(phi)
 
     e = random.randint(3, phi - 1)
     while nwd(e, phi) != 1:
         e = random.randint(3, phi - 1)
 
-    d = moduleInverse(e, phi)
+    print("e:::::")
+    print(e)
+
+    d = modinv(e, phi)
 
     publicKey = (n, e)
     privateKey = (n, d)
 
-    return(publicKey, privateKey)
+    return(Keys_Collection(publicKey, privateKey))
+
+m = 999
+keys = generate_keys()
+
+print("Publiczny klucz:::::")
+print(keys.publicKey)
+print("Prywatny klucz:::::: ")
+print(keys.privateKey)
+
+#szyfrowanie
+c = (m**keys.publicKey[1]) % keys.publicKey[0]
+print(m**keys.publicKey[1])
+print("zaszyfrowane")
+print(c)
+
+mm = (c**keys.privateKey[1]) % keys.privateKey[0]
+print("rosszyfrowane")
+print(mm)
+
